b303d6f44ff2287486ba69cadc582918573a0bbe
Catherine Maritz <catherinemaritz1@gmail.com>
2019-05-05 10:43:31 +0100

    conference badges and scheldules methods

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(attendees)
+  badge_array = []
+  attendees.each do |name|
+    badge_array << badge_maker(name)
+  end
+  badge_array
+end 
+
+def assign_rooms(attendees)
+  room_array = []
+  attendees.each_with_index do |name, index|
+    room_array << "Hello, #{name}! You'll be assigned to room #{index+1}!"
+  end
+  room_array
+end
+
+def printer(attendees)
+  badges_for_printing = batch_badge_creator(attendees)
+  rooms_for_printing = assign_rooms(attendees) 
+  badges_for_printing.each do |name|
+    puts name
+  end
+  rooms_for_printing.each do |name|
+    puts name
+  end 
+end 
